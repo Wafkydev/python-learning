0df17731aeacc02a3968ea27e556dc9eec8a8324
Wafky <13dit24f1027.psmza.edu.my>
2026-05-27 22:20:25 +0800

    Added list practice file

---
+++ ListWithfunctions.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+
+#select minimum data
+print(min(lego_parts))
+
+#select maximum data
+print(max(lego_parts))
+
