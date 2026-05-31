6c60d2b704fcc5ca1581f9cf2dfbc0a51066f5dc
Wafky <13dit24f1027.psmza.edu.my>
2026-05-31 09:14:22 +0800

    simple calculator lesson

---
+++ simpleCalc.py
@@ -0,0 +1,27 @@
+
+#addition
+def add(a, b):
+    return a + b
+
+#subtraction
+def sub(a, b):
+  return a - b
+
+#multiplication
+def multi(a, b):
+  return a * b
+
+#division
+def div(a, b):
+  return a / b
+
+#exponential
+def exp(a, b):
+  return a ** b
+  
+print(add(3, 5))
+print(sub(3, 5))
+print(multi(3, 5))
+print(div(3, 5))
+print(exp(3, 5))
+
